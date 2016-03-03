a930bb9586695b88b897cce8a31a683aac87fd7f
Teemu <pusanteemu@outlook.com>
2016-03-03 13:10:41 +0200

    PI_TEST_#1

--- header/gpioconstants.h
+++ header/gpioconstants.h
@@ -1,13 +1,14 @@
 
 #include <bcm2835.h>
 
-#define SUSPENSION_HIGH_PIN
-#define SUSPENSION_LOW_PIN
+#define SUSPENSION_HIGH_PIN RPI_GPIO_P1_10 // GPIO 15
+#define SUSPENSION_LOW_PIN RPI_GPIO_P1_11 // GPIO 17
 
-#define FAN_SPEED_1_PIN RPI_GPIO_P1_11 // Blinks on RPi Plug P1 pin 11 (which is GPIO pin 17)
-#define FAN_SPEED_2_PIN
-#define FAN_SPEED_3_PIN
-#define FAN_SPEED_4_PIN
+#define FAN_SPEED_1_PIN RPI_GPIO_P1_12 // GPIO 18
+ // Blinks on RPi Plug P1 pin 11 (which is GPIO pin 17)
+#define FAN_SPEED_2_PIN RPI_GPIO_P1_13 // GPIO 27
+#define FAN_SPEED_3_PIN RPI_GPIO_P1_15 // GPIO 22
+#define FAN_SPEED_4_PIN RPI_GPIO_P1_16 // GPIO 23
 
 #define DOWNFORCE_ON_PIN RPI_GPIO_P1_07 // GPIO 4
 
